4c0b20d79e35d858b7aa4aa4356834b7748c39d8
Ela Blechschmidt <ela@posteo.de>
2018-10-01 10:47:45 +0200

    delete content.json, add hbs linebreak helper, add new body

--- js/main.js
+++ js/main.js
@@ -1,7 +1,5 @@
 const Handlebars = require('handlebars');
 const fs = require('fs');
-const jsonData = require('../content.json');
-const pages = jsonData.pages;
 
 
 
@@ -9,20 +7,26 @@ var source = fs.readFileSync('../template/index.hbs', 'utf8', (err, htmlData) =>
 	if (err) throw err;
 });
 
-function writeFiles(page) {
+Handlebars.registerHelper('breaklines', function(text) {
+    text = Handlebars.Utils.escapeExpression(text);
+    text = text.replace(/(\r\n|\n|\r)/gm, '<br>');
+    return new Handlebars.SafeString(text);
+});
+
+var context = {
+			name: "index",
+			title: "Announcing Blockparty - an easy-to-use multi-network SSB client.",
+			h2: "What is blockparty?",
+			body: "blockparty is going to be an out-of-the-box desktop application that allows users to easily create their own decentralized, encrypted social network and invite others to join.\r\nUnlike Facebook for example, infrastructure and control over the data stay where they belong: in the hands of the users.\r\n\r\nWe use the secure-scuttlebutt protocol as starting point, but with blockparty it will be easier for non-technical people to start their own SSB-based network/group a.k.a blockparty.\r\nFor us, easy setup means a setup with few technical hurdles and good usability, so that anyone can join.\r\n\r\nOur goal is to make decentralized networks user friendly and therefore accessible for more people, even for the less technically savvy!\r\n\r\nblockparty leaves it up to users what they want their block party to be: social network, forum or family room.\r\nWe're building this free and open source project as a community effort because we believe in non-commercial, neutral, and fair mobile communication for everyone."
+}
+
+function writeFiles(context) {
 	var template = Handlebars.compile(source);
-	var generatedHTML = template(page);
-	fs.writeFileSync('../' + page.name + '.html', generatedHTML, 'utf8', (err) => {
+	var generatedHTML = template(context);
+	fs.writeFileSync('../' + context.name + '.html', generatedHTML, 'utf8', (err) => {
 		if (err) throw err;
 		});
 }
 
-pages.forEach(function(page){
-	writeFiles(page);
-});
 
-
-
-
-
-
+writeFiles(context);
